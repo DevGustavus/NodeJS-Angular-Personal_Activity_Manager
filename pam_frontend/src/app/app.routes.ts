7da0581f1ba2e38c4225f4307bfc3b6289e9c026
gustavo <gustavusinvictus@gmail.com>
2024-07-07 19:35:08 -0300

    completando telas home e about

--- pam_frontend/src/app/app.routes.ts
+++ pam_frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { UsersComponent } from './pages/users/users.component';
 import { CategoriesComponent } from './pages/categories/categories.component';
 import { ActivitiesComponent } from './pages/activities/activities.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'users', component: UsersComponent},
     {path: 'categories', component: CategoriesComponent},
-    {path: 'activities', component: ActivitiesComponent}
+    {path: 'activities', component: ActivitiesComponent},
+    {path: 'about', component: AboutComponent}
 ];
